7b288a4a5cb86dfef1b3f730abad5d9ee9f5570f
TANBIR AHAMED <tanbirahamedg@gmail.com>
2025-04-03 20:44:16 +0530

    feat:completed authentication based on lead and member

--- frontend/src/Pages/Dashboard/Tech/Widgets/RecentProposals.jsx
+++ frontend/src/Pages/Dashboard/Tech/Widgets/RecentProposals.jsx
@@ -10,7 +10,7 @@ export default function RecentProposals() {
   useEffect(() => {
     const fetchProposals = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/proposals"); // Update the endpoint if needed
+        const response = await axios.get("http://localhost:5001/api/proposals"); // Update the endpoint if needed
         setProposals(response.data.proposals); // ✅ Assuming the data is inside `proposals`
       } catch (error) {
         console.error("Error fetching proposals:", error);
