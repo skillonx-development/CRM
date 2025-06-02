7db03ec46cc4165137ebd1431d6d1ef42191a4c8
Vaibhavghugretkar <your-email@example.com>
2025-06-02 11:58:14 +0530

    fix:fixed fetching states

--- frontend/src/Pages/Dashboard/SalesDashboard/Widgets/ActiveOrders.jsx
+++ frontend/src/Pages/Dashboard/SalesDashboard/Widgets/ActiveOrders.jsx
@@ -11,7 +11,7 @@ const ActiveOrders = () => {
   useEffect(() => {
     const fetchProposals = async () => {
       try {
-        const response = await fetch("https://crm-r5rr.onrender.com/api/tech-proposals");
+        const response = await fetch("http://localhost:5001/api/tech-proposals");
         const data = await response.json();
 
         const activeOrders = data.filter(order => order.status !== "Completed" && order.status !== "Rejected");
